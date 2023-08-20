c1337dc7db8b2ccfd0bffee45bb2788af89d2130
Robert Wilcox <noreply@thewilcox.family>
2023-08-20 15:53:29 -0400

    feat: add part seven of chapter one exercise three

---
+++ ch1/ex3/Seven.java
@@ -0,0 +1,9 @@
+package ch1.ex3;
+
+public class Seven {
+  public static void main(String[] args) {
+    // This is a sample comment
+    System.out.Println("Hello, World!");
+    System.out.Println("How are you doing this evening?");
+  }
+}
