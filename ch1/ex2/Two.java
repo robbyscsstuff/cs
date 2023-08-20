7b4059446d75584e972be749582b0ab27f598d85
Robert Wilcox <noreply@thewilcox.family>
2023-08-20 15:36:30 -0400

    feat: add part two of chapter one exercise two

---
+++ ch1/ex2/Two.java
@@ -0,0 +1,8 @@
+package ch1.ex2;
+
+public class Two {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("How are you doing this evening?");
+  }
+}
