9aab0406c336fea26a0fef2dc4214ec633ef6c67
Robert Wilcox <noreply@thewilcox.family>
2023-08-20 15:49:42 -0400

    feat: add part four of chapter one exercise three

---
+++ ch1/ex3/Four.java
@@ -0,0 +1,9 @@
+package ch1.ex3;
+
+public class Four {
+  public void main(String[] args) {
+    // This is a sample comment
+    System.out.println("Hello, World!");
+    System.out.println("How are you doing this evening?");
+  }
+}
