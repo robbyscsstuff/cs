811fd8ff572c4dbbcfd21c621ddee73167d5f1c7
Robert Wilcox <noreply@thewilcox.family>
2023-08-20 15:54:45 -0400

    feat: add part eight of chapter one exercise three

---
+++ ch1/ex3/Eight.java
@@ -0,0 +1,9 @@
+package ch1.ex3;
+
+public class Eight {
+  public static void main(String[] args) {
+    // This is a sample comment
+    System.out.print("Hello, World!");
+    System.out.print("How are you doing this evening?");
+  }
+}
