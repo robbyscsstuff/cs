b63acdb7998343527c3d12c561da2590fac980dc
Robert Wilcox <noreply@thewilcox.family>
2023-08-20 15:56:33 -0400

    feat: add part ten of chapter one exercise three

---
+++ ch1/ex3/Ten.java
@@ -0,0 +1,9 @@
+package ch1.ex3;
+
+public class Ten {
+  public static void main(String[] args)) {
+    // This is a sample comment
+    System.out.println("Hello, World!");
+    System.out.println("How are you doing this evening?");
+  }
+}
